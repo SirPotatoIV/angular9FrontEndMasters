dfb0cfb155a85cd57dcdca8a6492cbe529e5c9e3
Jake O'Toole <otoolej33@gmail.com>
2020-07-14 16:25:08 -0500

    added a button to the template. Created first component method. Performed first event binding.

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -9,6 +9,10 @@ export class FirstComponentComponent implements OnInit {
 
   name = "jake";
   themeColor = "red";
+  updateColor = function(){
+    console.log("updated color!!")
+    this.themeColor = 'salmon';
+  }
 
   constructor() {}
 
